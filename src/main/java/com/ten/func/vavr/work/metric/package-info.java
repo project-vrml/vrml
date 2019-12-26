7ca66cfdc7e91185c9642f360d4a5d4f89815494
Kevin_T <596823919@qq.com>
2019-12-26 20:30:47 +0800

    init : init package

---
+++ src/main/java/com/ten/func/vavr/work/metric/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Metric common approaches
+ *
+ * @author shihaowang
+ * @date 2019/12/26
+ */
+package com.ten.func.vavr.work.metric;
---
+++ src/main/java/com/ten/func/vavr/work/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Use toolkit such as {@code Vavr} to model common approaches at work
+ *
+ * @author shihaowang
+ * @date 2019/12/26
+ */
+package com.ten.func.vavr.work;
